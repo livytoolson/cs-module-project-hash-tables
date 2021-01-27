40050344ff9f769b5e3ec4d2e69d159c52f1fd01
Olivia Toolson <livytoolson@gmail.com>
2021-01-27 08:51:05 -0700

    crack caesar, histo complete

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,43 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 
-# Your code here
+most_frequent = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
 
+with open('ciphertext.txt', 'r') as fileoutput:                 # open and read file
+    data = fileoutput.read()                                    # store output string into data variable
+    letters = [letter for letter in data]
+
+    cache = {}
+
+    for letter in letters:
+
+        if letter not in most_frequent:
+            continue
+        
+        if letter not in cache:
+            cache[letter] = 0
+        cache[letter] += 1
+
+    ordered_freq = [letter for letter in cache]
+
+    def sorter(letter):
+        return cache[letter]
+        
+    ordered_freq.sort(key = sorter, reverse = True)             # reverse = True --> greatest to least
+
+    story = ''
+
+    for letter in letters:
+
+        if letter not in ordered_freq:
+            story += letter
+            continue
+        
+        index = ordered_freq.index(letter)                      # find index of letter
+
+        new_letter = most_frequent[index]
+
+        story += new_letter
+
+    print(story)
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,35 @@
-# Your code here
+def histo(textfile):
+    with open(textfile, 'r') as fileoutput:             # open file, make it readable, store it as fileoutput variable
+        data = fileoutput.read()
+    
+    words = data.split()                                # split data file into an array
+    cache = {}                                          # counter
 
+    for word in words:
+        word = word.lower().strip('":;,.-+=/\\|[]}{()*^&')      # lowercase all words and strip punctuation
+
+        if word not in cache:                                   # if word is not in cache, add it, and append # for each word
+            cache[word] = []
+        cache[word].append('#')                                 # '#' is acting like word counter
+    
+    ordered_cache = [word for word in cache]
+
+    def sorter(word):
+        return cache[word]
+
+    ordered_cache.sort(key = sorter, reverse = True)
+
+    for word in ordered_cache:
+        print(f"{word:20} {''.join(cache[word])}")
+
+    # for word in ordered_cache:                                        # access key and value in cache
+    #     count_str = ''
+
+    #     for symbol in cache[word]:                                    # removes brackets from output
+    #         count_str += '#'
+
+        # print(f'{word:20}    {count_str}')
+    #   print("{:>20}{:>100}".format(word, count))
+    #   print(f"{i:3} {fib(i)}")
+
+histo('robin.txt')
